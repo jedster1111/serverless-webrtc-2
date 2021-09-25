862d3076579063015ff06d09f51b35e33ce2b90e
Jed Thompson <jedster1111@hotmail.co.uk>
2021-09-25 22:50:51 +0100

    Add local playground

---
+++ packages/playground/configs/webpack/common.js
@@ -0,0 +1,41 @@
+// shared config (dev and prod)
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  context: resolve(__dirname, "../../src"),
+  module: {
+    rules: [
+      {
+        test: [/\.tsx?$/],
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          "file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]",
+          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "index.html.ejs" })],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  performance: {
+    hints: false,
+  },
+};
---
+++ packages/playground/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello World!</h1>
+      <p>Foo to the barz! Wowwww</p>
+    </div>
+  );
+};
+
+export default App;
