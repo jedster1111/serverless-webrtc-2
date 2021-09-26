4fc16c9ba614183f33b70701495180706938ab27
Jed Thompson <jedster1111@hotmail.co.uk>
2021-09-26 02:51:16 +0100

    Delete accidentally commited build artifacts

--- packages/playground/src/components/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const App = () => {
-    return (React.createElement("div", { className: "app" },
-        React.createElement("h1", null, "Hello World!"),
-        React.createElement("p", null, "Foo to the barz! Wowwww")));
-};
-export default App;
-//# sourceMappingURL=App.js.map
