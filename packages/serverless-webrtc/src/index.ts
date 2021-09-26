c22917568e992523d05302e73287af43f99b763d
Jed Thompson <jedster1111@hotmail.co.uk>
2021-09-27 00:08:49 +0100

    Revert "Remove ice server again"
    This reverts commit e28b3e88a0efb8a47796d8404f0eacdd5124080e.

--- packages/serverless-webrtc/src/index.ts
+++ packages/serverless-webrtc/src/index.ts
@@ -17,7 +17,7 @@ type MessageWithoutData<T extends string> = {
 };
 
 const defaultRTCConfig: RTCConfiguration = {
-  iceServers: [],
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
 
 export const useServerlessWebRTC = <
